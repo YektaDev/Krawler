c3915707e23e5e566235c8d35b7e9a6941b8b962
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-14 16:51:07 +0330

    conf: Add `sqliteJdbc` and `exposed-kotlin-datetime`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 val ktorVersion: String by project
 val okioVersion: String by project
+val sqliteJdbcVersion: String by project
 val exposedVersion: String by project
 val ktomlVersion: String by project
 
@@ -22,9 +23,12 @@ dependencies {
 
     implementation("com.squareup.okio:okio:$okioVersion")
 
+    implementation("org.xerial:sqlite-jdbc:$sqliteJdbcVersion")
+
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
 
     implementation("com.akuleshov7:ktoml-core:$ktomlVersion")
     implementation("com.akuleshov7:ktoml-file:$ktomlVersion")
