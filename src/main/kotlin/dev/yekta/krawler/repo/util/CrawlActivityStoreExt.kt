bba49e1576ea814b4070e06dae5d7308ea2b7289
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-17 19:47:52 +0330

    fix(CrawlActivityStoreExt): Make concurrent

--- src/main/kotlin/dev/yekta/krawler/repo/util/CrawlActivityStoreExt.kt
+++ src/main/kotlin/dev/yekta/krawler/repo/util/CrawlActivityStoreExt.kt
@@ -2,9 +2,8 @@ package dev.yekta.krawler.repo.util
 
 import dev.yekta.krawler.model.CrawlingSessionID
 import dev.yekta.krawler.repo.CrawlActivityStore
-import kotlinx.datetime.Clock.System.now
 
-fun CrawlActivityStore.start(session: CrawlingSessionID) = addStart(session, now().epochSeconds)
-fun CrawlActivityStore.stop(session: CrawlingSessionID) = addStop(session, now().epochSeconds)
-fun CrawlActivityStore.pause(session: CrawlingSessionID) = addPause(session, now().epochSeconds)
-fun CrawlActivityStore.resume(session: CrawlingSessionID) = addResume(session, now().epochSeconds)
+suspend fun CrawlActivityStore.start(session: CrawlingSessionID) = addStart(session, currentEpochSeconds())
+suspend fun CrawlActivityStore.stop(session: CrawlingSessionID) = addStop(session, currentEpochSeconds())
+suspend fun CrawlActivityStore.pause(session: CrawlingSessionID) = addPause(session, currentEpochSeconds())
+suspend fun CrawlActivityStore.resume(session: CrawlingSessionID) = addResume(session, currentEpochSeconds())
