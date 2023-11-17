3e11cfe182577f76ea24ccd34788ea7d97733567
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-17 19:51:35 +0330

    feat(CrawlErrorStoreImp): Implement

--- src/main/kotlin/dev/yekta/krawler/repo/imp/CrawlErrorStoreImp.kt
+++ src/main/kotlin/dev/yekta/krawler/repo/imp/CrawlErrorStoreImp.kt
@@ -2,9 +2,19 @@ package dev.yekta.krawler.repo.imp
 
 import dev.yekta.krawler.model.CrawlingSessionID
 import dev.yekta.krawler.repo.CrawlErrorStore
+import dev.yekta.krawler.repo.orm.DBManager
+import dev.yekta.krawler.repo.orm.table.CrawlErrors
+import org.jetbrains.exposed.sql.insert
 
-class CrawlErrorStoreImp:CrawlErrorStore {
-    override fun add(sessionID: CrawlingSessionID, url: String, error: String) {
-        TODO("Not yet implemented")
+class CrawlErrorStoreImp : CrawlErrorStore {
+    override suspend fun add(session: CrawlingSessionID, epochSeconds: Long, url: String, error: String) {
+        DBManager.transaction {
+            CrawlErrors.insert {
+                it[this.sessionId] = session.value
+                it[this.atEpochSeconds] = epochSeconds
+                it[this.url] = url
+                it[this.error] = error
+            }
+        }
     }
 }
