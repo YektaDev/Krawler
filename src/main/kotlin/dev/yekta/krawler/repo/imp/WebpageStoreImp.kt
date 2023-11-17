a2df621d3cc8382988a518290a6e4b2eea627f19
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-17 19:51:54 +0330

    feat(WebpageStoreImp): Implement

--- src/main/kotlin/dev/yekta/krawler/repo/imp/WebpageStoreImp.kt
+++ src/main/kotlin/dev/yekta/krawler/repo/imp/WebpageStoreImp.kt
@@ -2,9 +2,19 @@ package dev.yekta.krawler.repo.imp
 
 import dev.yekta.krawler.model.CrawlingSessionID
 import dev.yekta.krawler.repo.WebpageStore
+import dev.yekta.krawler.repo.orm.DBManager
+import dev.yekta.krawler.repo.orm.table.Webpages
+import org.jetbrains.exposed.sql.insert
 
 class WebpageStoreImp : WebpageStore {
-    override fun add(session: CrawlingSessionID, url: String, html: String) {
-        TODO("Not yet implemented")
+    override suspend fun add(session: CrawlingSessionID, epochSeconds: Long, url: String, html: String) {
+        DBManager.transaction {
+            Webpages.insert {
+                it[this.sessionId] = session.value
+                it[this.atEpochSeconds] = epochSeconds
+                it[this.url] = url
+                it[this.html] = html
+            }
+        }
     }
 }
