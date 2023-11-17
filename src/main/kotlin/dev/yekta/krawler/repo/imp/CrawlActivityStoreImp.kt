824c7be8df2a6e4a3ca66b1f0b7cfa2a990154e0
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-17 19:51:23 +0330

    feat(CrawlActivityStoreImp): Implement

--- src/main/kotlin/dev/yekta/krawler/repo/imp/CrawlActivityStoreImp.kt
+++ src/main/kotlin/dev/yekta/krawler/repo/imp/CrawlActivityStoreImp.kt
@@ -2,21 +2,23 @@ package dev.yekta.krawler.repo.imp
 
 import dev.yekta.krawler.model.CrawlingSessionID
 import dev.yekta.krawler.repo.CrawlActivityStore
+import dev.yekta.krawler.repo.orm.DBManager
+import dev.yekta.krawler.repo.orm.table.CrawlActivities
+import org.jetbrains.exposed.sql.insert
 
-class CrawlActivityStoreImp:CrawlActivityStore {
-    override fun addStart(session: CrawlingSessionID, epochSeconds: Long) {
-        TODO("Not yet implemented")
+class CrawlActivityStoreImp : CrawlActivityStore {
+    private suspend fun add(session: CrawlingSessionID, epochSeconds: Long, event: String) {
+        DBManager.transaction {
+            CrawlActivities.insert {
+                it[this.sessionId] = session.value
+                it[this.atEpochSeconds] = epochSeconds
+                it[this.type] = event
+            }
+        }
     }
 
-    override fun addStop(session: CrawlingSessionID, epochSeconds: Long) {
-        TODO("Not yet implemented")
-    }
-
-    override fun addPause(session: CrawlingSessionID, epochSeconds: Long) {
-        TODO("Not yet implemented")
-    }
-
-    override fun addResume(session: CrawlingSessionID, epochSeconds: Long) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun addStart(session: CrawlingSessionID, epochSeconds: Long) = add(session, epochSeconds, "Start")
+    override suspend fun addStop(session: CrawlingSessionID, epochSeconds: Long) = add(session, epochSeconds, "Stop")
+    override suspend fun addPause(session: CrawlingSessionID, epochSeconds: Long) = add(session, epochSeconds, "Pause")
+    override suspend fun addResume(session: CrawlingSessionID, epochSeconds: Long) = add(session, epochSeconds, "Resume")
 }
