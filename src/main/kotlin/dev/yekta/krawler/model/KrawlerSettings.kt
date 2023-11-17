bc070d04579e20bfc53c2fe4074b2788704e14d5
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-18 00:20:54 +0330

    feat(KrawlerSettings): Add more properties + add property validation + set Log.verbose at init
    Took 1 minute

--- src/main/kotlin/dev/yekta/krawler/model/KrawlerSettings.kt
+++ src/main/kotlin/dev/yekta/krawler/model/KrawlerSettings.kt
@@ -1,5 +1,6 @@
 package dev.yekta.krawler.model
 
+import dev.yekta.krawler.log.Log
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -8,11 +9,31 @@ data class KrawlerSettings(
     val filter: CrawlingFilter,
     val depth: Int = 4,
     val maxPages: Int? = null,
-    val maxDuration: Int? = null,
-    val maxPageSize: Int? = null,
-    val userAgent: String = "Krawler",
+    val maxPageSizeKb: Int? = null,
     val concurrentConnections: Int = 4,
-    val outputDir: String = "output",
-    val outputFormat: String = "json",
     val verbose: Boolean = false,
-)
+    val shouldFollowRedirects: Boolean = true,
+    val userAgent: String = "Krawler",
+    val connectTimeoutMs: Long = 10_000,
+    val readTimeoutMs: Long = 10_000,
+    val retriesOnServerError: Int = 0,
+    val customHeaders: List<Pair<String, String>>? = listOf("saf" to "dsfsdf"),
+) {
+    val maxPageSizeBytes = maxPageSizeKb?.times(1000)
+
+    init {
+        require(seeds.isNotEmpty()) { "No Seeds are provided!" }
+        require(depth <= 100) { "Depth must be less than or equal to 100." }
+        require(depth > 0) { "Depth must be positive." }
+        require(maxPages == null || maxPages > 0) { "`maxPages` must be positive if it's provided." }
+        require(maxPageSizeKb == null || maxPageSizeKb > 0) { "`maxPageSizeKb` must be positive if it's provided." }
+        require(concurrentConnections > 0) { "Concurrent connections must be positive!" }
+        require(concurrentConnections <= 10_000) { "Concurrent connections cannot be more than 10K!" }
+        require(connectTimeoutMs >= 10) { "`connectTimeoutMs` cannot be less than 10!" }
+        require(readTimeoutMs >= 10) { "`readTimeoutMs` cannot be less than 10!" }
+        require(retriesOnServerError >= 0) { "`retriesOnServerError` cannot be negative!" }
+        require(customHeaders?.isEmpty() != true) { "`customHeaders` cannot be empty once it's provided." }
+
+        Log.verbose = verbose
+    }
+}
