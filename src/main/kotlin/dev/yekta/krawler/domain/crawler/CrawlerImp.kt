2c22e37506b6c5dc97f39f95b674e17899e071f2
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-18 07:49:51 +0330

    feat(CrawlerImp): Update
    Took 26 seconds

--- src/main/kotlin/dev/yekta/krawler/domain/crawler/CrawlerImp.kt
+++ src/main/kotlin/dev/yekta/krawler/domain/crawler/CrawlerImp.kt
@@ -1,5 +1,7 @@
 package dev.yekta.krawler.domain.crawler
 
+import dev.yekta.krawler.console.info
+import dev.yekta.krawler.console.verbose
 import dev.yekta.krawler.domain.fetcher.ConcurrentFetcher
 import dev.yekta.krawler.domain.fetcher.ConcurrentFetcherImp
 import dev.yekta.krawler.domain.fetcher.FetchResult
@@ -17,9 +19,9 @@ import dev.yekta.krawler.model.CrawlingSessionID
 import dev.yekta.krawler.model.KrawlerSettings
 import dev.yekta.krawler.repo.Repo
 import dev.yekta.krawler.repo.util.add
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.datetime.Clock
+import okio.utf8Size
 import kotlin.math.min
 
 class CrawlerImp(
@@ -31,7 +33,15 @@ class CrawlerImp(
 ) : Crawler {
     private val pool: UrlPool = UrlPoolImp()
     private val scheduler: Scheduler = FifoScheduler(sessionId, repo.state)
-    private val fetcher: ConcurrentFetcher = ConcurrentFetcherImp(settings.concurrentConnections)
+    private val fetcher: ConcurrentFetcher = ConcurrentFetcherImp(
+        maxConnections = settings.concurrentConnections,
+        shouldFollowRedirects = settings.shouldFollowRedirects,
+        userAgent = settings.userAgent,
+        connectTimeoutMs = settings.connectTimeoutMs,
+        readTimeoutMs = settings.readTimeoutMs,
+        retriesOnServerError = settings.retriesOnServerError,
+        customHeaders = settings.customHeaders,
+    )
     private val extractor: UrlExtractor = UrlExtractorImp()
 
     override fun crawl(seeds: List<String>) = scope.launch {
@@ -44,19 +54,59 @@ class CrawlerImp(
     }
 
     private suspend fun run() {
+        var i = 0
+        val hasReachedPageLimit = {
+            when (val max = settings.maxPages) {
+                null -> false
+                else -> ++i >= max
+            }
+        }
+
         var url = scheduler.next()
         while (url != null) {
             val u = url
             fetcher.fetch(u.url) { result -> handleFetchResult(u.depth, u.url, result) }
+
+            if (hasReachedPageLimit()) break
             url = scheduler.next()
+            val start = Clock.System.now().epochSeconds
+            while (url == null && Clock.System.now().epochSeconds - start < settings.connectTimeoutMs + settings.readTimeoutMs) {
+                info("Waiting for active crawling processes to provide new URLs...")
+                delay(1000)
+                url = scheduler.next()
+            }
         }
+
+        informEndOfCrawling(reachedMaxPageLimit = url != null, totalCrawled = i)
+        finish()
+    }
+
+    private fun informEndOfCrawling(reachedMaxPageLimit: Boolean, totalCrawled: Int) {
+        when (reachedMaxPageLimit) {
+            true -> info("Crawling reached its max page count limit.")
+            false -> info("All discovered URLs are processed. ")
+        }
+        info("The crawling process is over.")
+        info("Total Crawled URLs: $totalCrawled")
     }
 
     private suspend fun handleFetchResult(depth: Int, url: String, result: FetchResult) {
         val minDepth = updateAndGetMinDepth(url, depth)
         when (result) {
             is Html -> {
+                if (settings.maxPageSizeBytes != null && result.html.utf8Size() > settings.maxPageSizeBytes) {
+                    info("[$url] has ${result.html.utf8Size()} bytes, skipping it.")
+                    return
+                }
+
                 repo.webpage.add(sessionId, url, result.html)
+
+                if (settings.depth <= minDepth) {
+                    verbose("[$url] URL is too deep from seeds to be used to extract new links, skipping link extraction.")
+                    return
+                }
+                verbose("[$url] Extracting URLs")
+
                 scheduleNewUrls(
                     depth = minDepth + 1,
                     urls = extractor.extract(
