9b10a9810d4f8a2093aeaf2cd3c86c7ee89aca83
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-18 17:05:44 +0330

    feat(WebpageStore): Add `total()` & `averageHtmlLength()`
    Took 2 hours 54 minutes

--- src/main/kotlin/dev/yekta/krawler/repo/WebpageStore.kt
+++ src/main/kotlin/dev/yekta/krawler/repo/WebpageStore.kt
@@ -4,4 +4,7 @@ import dev.yekta.krawler.model.CrawlingSessionID
 
 interface WebpageStore {
     suspend fun add(session: CrawlingSessionID, epochSeconds: Long, url: String, html: String)
+
+    suspend fun total(session: CrawlingSessionID): Long
+    suspend fun averageHtmlLength(session: CrawlingSessionID): Double
 }
--- src/main/kotlin/dev/yekta/krawler/repo/imp/WebpageStoreImp.kt
+++ src/main/kotlin/dev/yekta/krawler/repo/imp/WebpageStoreImp.kt
@@ -5,6 +5,7 @@ import dev.yekta.krawler.repo.WebpageStore
 import dev.yekta.krawler.repo.orm.DBManager
 import dev.yekta.krawler.repo.orm.table.Webpages
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
 
 class WebpageStoreImp : WebpageStore {
     override suspend fun add(session: CrawlingSessionID, epochSeconds: Long, url: String, html: String) {
@@ -16,5 +17,13 @@ class WebpageStoreImp : WebpageStore {
                 it[this.html] = html
             }
         }
+    }
+
+    override suspend fun total(session: CrawlingSessionID): Long = DBManager.transaction {
+        Webpages.select { Webpages.sessionId eq session.value }.count()
+    }
+
+    override suspend fun averageHtmlLength(session: CrawlingSessionID): Double = DBManager.transaction {
+        Webpages.select { Webpages.sessionId eq session.value }.map { it[Webpages.html].length }.average()
     }
 }
