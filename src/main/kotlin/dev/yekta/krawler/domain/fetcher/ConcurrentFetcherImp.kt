af8cb2a5e5947551183eef18d1e9c2e0fa7f6d7d
Ali Khaleqi Yekta <me@yekta.dev>
2023-11-18 08:10:09 +0330

    feat(ConcurrentFetcherImp): Update
    Took 29 seconds

--- src/main/kotlin/dev/yekta/krawler/domain/fetcher/ConcurrentFetcherImp.kt
+++ src/main/kotlin/dev/yekta/krawler/domain/fetcher/ConcurrentFetcherImp.kt
@@ -7,6 +7,8 @@ import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit.MILLISECONDS
@@ -51,28 +53,60 @@ class ConcurrentFetcherImp(
         }
     }
 
-    override suspend fun fetch(url: String, onRead: suspend (FetchResult) -> Unit) = launchOrSuspendRead(url, onRead)
+    override suspend fun fetch(url: String, onRead: suspend (FetchResult) -> Unit) =
+        launchOrSuspendRead(url, onRead)
 
-    private suspend inline fun launchOrSuspendRead(url: String, crossinline onRead: suspend (FetchResult) -> Unit) {
-        if (activeConnections.value >= maxConnections) return onRead(read(url))
-        activeConnections.value++
-        client.launch {
-            val readResult = read(url)
-            activeConnections.value--
-            onRead(readResult)
+    private suspend inline fun launchOrSuspendRead(url: String, crossinline onRead: suspend (FetchResult) -> Unit) =
+        when {
+            activeConnections.value >= maxConnections -> launchRequest { onRead(read(url)) }
+            else -> {
+                activeConnections.value++
+                launchRequest {
+                    val readResult = read(url)
+                    activeConnections.value--
+                    onRead(readResult)
+                }
+            }
+        }
+
+    private suspend inline fun launchRequest(crossinline block: suspend CoroutineScope.() -> Unit) {
+        client.launch(SupervisorJob()) {
+            block()
         }
     }
 
     private suspend fun read(url: String): FetchResult = try {
-        Log.v("GET: $url")
+        Log.v("[$url] Get")
         val response = client.get(urlString = url)
-        when {
-            response.status.isSuccess().not() -> readError(url, response.status.description)
-            response.contentType() != ContentType.Text.Html -> FetchResult.NotHtml(response.contentType()?.contentType)
-            else -> FetchResult.Html(response.bodyAsText())
-        }
+        Log.v("[$url] Get Response Code: ${response.status}")
+
+        response.toFetchResult(url)
     } catch (e: Throwable) {
         readError(url, error = e.message ?: e.stackTraceToString())
+    }
+
+    private suspend fun HttpResponse.toFetchResult(url: String): FetchResult {
+        val type = contentType()?.contentType
+        return when {
+            !status.isSuccess() -> readError(url, status.description)
+            type != "text" -> FetchResult.NotHtml(type)
+            else -> {
+                val body = bodyAsText()
+                when (isContentHtml(body)) {
+                    true -> FetchResult.Html(body)
+                    false -> FetchResult.NotHtml(type)
+                }
+            }
+        }
+    }
+
+    private fun isContentHtml(content: String): Boolean {
+        val trimmed = content.trimStart()
+        return when {
+            trimmed.startsWith("<!doctype html>", ignoreCase = true) -> true
+            trimmed.contains("<html>", ignoreCase = true) -> true
+            else -> false
+        }
     }
 
     private fun readError(url: String, error: String): FetchResult {
